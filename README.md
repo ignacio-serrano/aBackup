e87b0ab7ad4eb1861d5be8cf1e7c29d2eddeef87
ignacio-serrano <asdftglkjyh@yahoo.es>
2017-03-06 19:53:36 +0100

    Added some initial stuff to project.

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# aBackup
+aBackup
+=======
+
 A quick and dirty backup tool for Windows
+
+TODO:
+-----
+
+  * Everything else is to be done.
+    * ...
+  * \[DONE\] Create this repository.
+
+Project layout
+--------------
+  * `src`: Here it is where source code is.
+    * `main`: Here it is where source code meant to be part of the application lives. It is here just in case I want to add a `test` directory some day.
+
+
+---
+Below, a markdown cheatsheet.
+
+Heading
+=======
+Sub-heading
+-----------
+### Another deeper heading
+
+---
+
+Paragraphs are separated
+by a blank line.
+
+Two spaces at the end of a line leave a  
+line break.
+
+Text attributes _italic_, *italic*, __bold__, **bold**, `monospace`.
+
+Bullet list:
+
+  * apples
+  * oranges
+  * pears
+
+Numbered list:
+
+  1. apples
+  2. oranges
+  3. pears
+
+A [link](http://example.com).
+
+```javascript
+function {
+  //Javascript highlighted code block.
+}
+```
+
+    {
+    Code block without highlighting.
+    }
---
+++ src/main/rinse-n-repeat.bat
@@ -0,0 +1,148 @@
+::<?xml version="1.0" encoding="Cp850"?><contenido><![CDATA[
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: PROGRAM ®rinse-n-repeat¯
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::    It allows to test code you are not really sure how/if would work.
+::
+:: USAGE:
+::    rinse-n-repeat.bat
+::
+:: DEPENDENCIES: :findOutInstall
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+@ECHO OFF
+SETLOCAL EnableDelayedExpansion
+:::::::::::::::::::::::::::::::::: PREPROCESS ::::::::::::::::::::::::::::::::::
+:: This variable will be used to manage the final ERRORLEVEL of the program.
+SET errLvl=0
+
+:: This program admits no parameters right now.
+REM CALL :parseParameters %*
+REM IF ERRORLEVEL 1 (
+REM 	SET errLvl=1
+REM 	GOTO :exit
+REM )
+CALL :findOutInstall "%~0" installDir
+REM CALL :loadProperties "%installDir%\git-backup.properties"
+
+:::::::::::::::::::::::::::::::::::: PROCESS :::::::::::::::::::::::::::::::::::
+
+GOTO :exit
+:::::::::::::::::::::::::::::::::: POSTPROCESS :::::::::::::::::::::::::::::::::
+:exit
+
+EXIT /B %errLvl% & ENDLOCAL
+
+:::::::::::::::::::::::::::::::::: SUBROUTINES :::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: BEGINNING: SUBROUTINE ®findOutInstall¯
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::    Computes the absolute path of the .bat passed as parameter. This 
+:: subroutine helps identify the installation directory of .bat script which 
+:: invokes it.
+:: 
+:: USAGE: 
+::    CALL :findOutInstall "%~0" ®retVar¯
+:: WHERE...
+::    ®retVar¯: Name of a variable (existent or not) by means of which the 
+::              directory will be returned.
+::
+:: DEPENDENCIES: :removeFileName
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:findoutInstall
+SETLOCAL
+SET retVar=%2
+
+SET installDir=%~$PATH:1
+IF "%installDir%" EQU "" (
+	SET installDir=%~f1
+)
+
+CALL :removeFileName "%installDir%" _removeFileName
+SET installDir=%_removeFileName%
+
+ENDLOCAL & SET %retVar%=%installDir%
+EXIT /B 0
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: END: SUBROUTINE ®findOutInstall¯
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: BEGINNING: SUBROUTINE ®removeFileName¯
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::    Removes the file name from a path.
+::
+:: USAGE: 
+::    CALL :removeFileName ®["]path["]¯ ®retVar¯
+:: WHERE...
+::    ®["]path["]¯: Path from which the file name is to be removed. If the path
+::                  contains white spaces, it must be enclosed in double quotes.
+::                  It is optional otherwise.
+::    ®retVar¯:     Name of a variable (existent or not) by means of which the 
+::                  directory will be returned.
+::
+:: DEPENDENCIES: NONE
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:removeFileName
+SETLOCAL
+SET retVar=%2
+SET path=%~dp1
+
+PUSHD %path%
+SET path=%CD%
+POPD
+
+ENDLOCAL & SET %retVar%=%path%
+EXIT /B 0
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: END: SUBROUTINE ®removeFileName¯
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: BEGINNING: SUBROUTINE ®loadProperties¯
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::    Reads a propreties file and loads it in environment variables.
+::
+:: USAGE: 
+::    CALL :loadProperties "®properties file path¯"
+:: WHERE...
+::    ®properties file path¯: Absolute or relative path of the properties file 
+::                            to read.
+::
+:: DEPENDENCIES: NONE
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:loadProperties
+FOR /F "usebackq eol=# tokens=1 delims=ª" %%i IN ("%~1") DO (
+	SET %%i
+)
+EXIT /B 0
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: END: SUBROUTINE ®loadProperties¯
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: BEGINNING: SUBROUTINE ®validateProgramAvailable¯
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::    Verifies whether an executable file is present in PATH environment 
+:: variable.
+::
+:: USAGE: 
+::    CALL :validateProgramAvailable ®["]program["]¯
+:: WHERE...
+::    ®["]program["]¯: Name of the executable file. If the file name contains 
+::                     white spaces, it must be enclosed in double quotes. It 
+::                     is optional otherwise.
+::
+:: DEPENDENCIES: NONE
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:validateProgramAvailable
+SETLOCAL
+SET errLvl=0
+IF "%~$PATH:1" == "" (
+	ECHO ERROR: Program ®%1¯ cannot be found in PATH. git-backup requires a ®%1¯ installation to work.
+	SET errLvl=1
+)
+ENDLOCAL & EXIT /B %errLvl%
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: END: SUBROUTINE ®validateProgramAvailable¯
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::]]></contenido>
